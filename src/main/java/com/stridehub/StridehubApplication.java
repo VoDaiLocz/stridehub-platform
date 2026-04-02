b06e0e26f0b69d2b9629fddb17c0d6f771b3a923
VoDaiLocz <locv2659@gmail.com>
2026-04-02 09:50:21 +0700

    chore: initialize spring boot workspace

---
+++ src/main/java/com/stridehub/StridehubApplication.java
@@ -0,0 +1,13 @@
+package com.stridehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StridehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StridehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stridehub/StridehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stridehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StridehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
